7b63df4e3078848a1b9a69673960ac24915d0404
ArhabSafwan <asafwan72@gmail.com>
2022-06-29 11:58:32 +0600

    old

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 let btn = document.getElementById('btn');
 let display1 = document.getElementById('display1');
+let display2 = document.getElementById('display2');
 
-var chat= ['hello','Zuhan ekta horny pola','tawssef vhai is best','tonmoy vhai zulumkari','sharif vhai osthir','tareq vhai extra terrstrial','foysal vhai boy'];
+var chat = ['hello', 'Zuhan ekta horny pola', 'tawssef vhai is best', 'tonmoy vhai zulumkari', 'sharif vhai osthir', 'tareq vhai extra terrstrial', 'foysal vhai boy'];
 
-btn.addEventListener('click',function(){
-     var randomchat = chat[Math.floor(Math.random()*chat.length)]
+btn.addEventListener('click', function () {
+    var randomchat = chat[Math.floor(Math.random() * chat.length)]
     display1.innerHTML = randomchat;
 
-let display = document.getElementById('userbox').value;
-document.getElementById('display2').innerHTML=display;
-document.getElementById('userbox').value='';
+    let display = document.getElementById('userbox').value;
+    document.getElementById('display2').innerHTML = display;
+    document.getElementById('userbox').value = '';
 
- })
+
+
+
+})
 
 
 
